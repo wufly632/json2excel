277fa9f5880e5589c364ce349a252c71e401e5fb
wufly <wfxykzd@163.com>
2023-04-19 23:16:12 +0800

    json转excel工具

---
+++ go.mod
@@ -0,0 +1,21 @@
+module json2excel
+
+go 1.19
+
+require (
+	github.com/tidwall/gjson v1.14.4
+	github.com/xuri/excelize/v2 v2.7.1
+)
+
+require (
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/tidwall/gjson"
+	"github.com/xuri/excelize/v2"
+	"os"
+)
+
+// Excel配置
+type ExcelConf struct {
+	SheetName  string  `json:"SheetName"`
+	Fields     []Field `json:"Fields"` // 字段-列号映射
+	OutputPath string  `json:"OutputPath"`
+}
+
+type Field struct {
+	FieldName string
+	FieldPath string
+}
+
+// 读取Excel配置
+func readExcelConfig() ExcelConf {
+	data, _ := os.ReadFile("conf.json")
+	var conf ExcelConf
+	json.Unmarshal(data, &conf)
+	return conf
+}
+
+func main() {
+	// 读取配置
+	conf := readExcelConfig()
+
+	// 解析JSON
+	// 读取data.json
+	dataJSON, _ := os.ReadFile("data.json")
+
+	// 新建Excel
+	f := excelize.NewFile()
+	_, err := f.NewSheet(conf.SheetName)
+	if err != nil {
+		panic("创建sheet失败")
+	}
+
+	for col, field := range conf.Fields {
+		cell, err := excelize.CoordinatesToCellName(col+1, 1)
+		if err != nil {
+			panic(err)
+		}
+		f.SetCellValue(conf.SheetName, cell, field.FieldName)
+	}
+
+	// 插入数据
+	colIndex := 0
+	gjson.ParseBytes(dataJSON).ForEach(func(key, value gjson.Result) bool {
+		colIndex++
+		for col, field := range conf.Fields {
+
+			cell, _ := excelize.CoordinatesToCellName(col+1, colIndex+1)
+			excelValue := gjson.Get(value.String(), field.FieldPath).String()
+
+			f.SetCellValue(conf.SheetName, cell, excelValue)
+		}
+		return true
+	})
+	// 保存Excel
+	f.SaveAs(conf.OutputPath)
+}
